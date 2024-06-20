f67c7bd88af8cee98c500fd7adf8ef406a8d5129
FlorianSp2000 <florian.sprick@hotmail.com>
2024-06-20 21:17:54 +0200

    add save option and color palette to detect script

---
+++ constants.py
@@ -0,0 +1,14 @@
+""" Holds all constants used in detection scripts. """
+
+CLASS_COLOR_MAP = {
+    'boat': (0, 255, 0),  # Green
+    'buoy': (255, 0, 0),  # Blue
+    'cruise ship': (0, 0, 255),  # Red
+    'ferry boat': (255, 255, 0),  # Cyan
+    'freight boat': (255, 0, 255),  # Magenta
+    'gondola': (0, 255, 255),  # Yellow
+    'inflatable boat': (128, 128, 128),  # Gray
+    'kayak': (128, 128, 0),  # Olive
+    'paper boat': (0, 128, 128),  # Teal
+    'sailboat': (128, 0, 128),  # Purple
+}
--- detect.py
+++ detect.py
@@ -1,11 +1,13 @@
 """ detect script based on ultralytics yolov10 model"""
 import os
-import sys
 import argparse
 import cv2
+
 from ultralytics import YOLO
 
-def detect_boats_in_image(image_path: str, model_path: str) -> None:
+from constants import CLASS_COLOR_MAP
+
+def detect_boats_in_image(image_path: str, model_path: str, save_image: bool) -> None:
     """
     Detect boats in an image using the YOLO model.
 
@@ -19,36 +21,39 @@ def detect_boats_in_image(image_path: str, model_path: str) -> None:
     if image is None:
         print(f"Could not read image {image_path}")
         return
-
-    results = model(image)
+    
+    result = model(image)[0]
 
     # Draw bounding boxes and labels on the image
-    for result in results.xyxy[0]:
-        x1, y1, x2, y2, confidence, class_id = result[:6]
+    for i in range(len(result.boxes.xyxy)):
+        x1, y1, x2, y2 = result.boxes.xyxy[i]
+        confidence, class_id = result.boxes.conf[i], result.boxes.cls[i]
         label = model.names[int(class_id)]
-        if label == 'boat':  # Assuming 'boat' is the label for boats
-            cv2.rectangle(image, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
-            cv2.putText(image, f'{label} {confidence:.2f}', (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+
+        bounding_box_color = CLASS_COLOR_MAP.get(label, (0, 255, 0))
+        cv2.rectangle(image, (int(x1), int(y1)), (int(x2), int(y2)), bounding_box_color, 2)
+        cv2.putText(image, f'{label} {confidence:.2f}', (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, bounding_box_color, 2)
 
     # Save or display the image
-    base, ext = os.path.splitext(image_path)
-    output_path = f"{base}_detected{ext}"
-    cv2.imwrite(output_path, image)
-    print(f"Detection results saved to {output_path}")
+    if save_image:
+        base, ext = os.path.splitext(image_path)
+        output_path = f"{base}_detected{ext}"
+        cv2.imwrite(output_path, image)
+        print(f"Detection results saved to {output_path}")
     cv2.imshow('Detected Boats', image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-def detect_boats_in_video(video_path: str, model_path: str) -> None:
+def detect_boats_in_video(video_path: str, model_path: str, save_video: bool) -> None:
     """
-    Detect boats in a video using the YOLO model.
+    Detect boats in a video using ultralytics YOLO model.
 
     :param video_path: Path to the video file.
     :param model_path: Path to the trained YOLO model file.
+    :param save_video: Whether to save the processed video or not.
     """
     model = YOLO(model_path)
 
-    # Open the video file
     cap = cv2.VideoCapture(video_path)
     if not cap.isOpened():
         print(f"Error opening video file {video_path}")
@@ -58,27 +63,40 @@ def detect_boats_in_video(video_path: str, model_path: str) -> None:
     frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fps = int(cap.get(cv2.CAP_PROP_FPS))
-    output_path = 'output_video.avi'
 
-    # Define the codec and create VideoWriter object
-    out = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc('M','J','P','G'), fps, (frame_width, frame_height))
+    if save_video:
+        # Prepare output video
+        model_name = os.path.splitext(os.path.basename(model_path))[0]
+        # Save video as same file type as input video
+        base_name, ext = os.path.splitext(os.path.basename(video_path))
+        
+        output_file = f"{base_name}_detected_{model_name}.{ext}"
+        if not os.path.exists('evaluation_results'):
+            os.makedirs('evaluation_results')
+        output_path = os.path.join('evaluation_results', output_file)
+
+        # Define the codec and create VideoWriter object
+        out = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc('M','J','P','G'), fps, (frame_width, frame_height))
 
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             break
 
-        results = model(frame)
+        results = model(frame)[0]
 
         # Draw bounding boxes and labels on the frame
-        for result in results.xyxy[0]:
-            x1, y1, x2, y2, confidence, class_id = result[:6]
+        for i in range(len(results.boxes.xyxy)):
+            x1, y1, x2, y2 = results.boxes.xyxy[i]
+            confidence, class_id = results.boxes.conf[i], results.boxes.cls[i]
             label = model.names[int(class_id)]
-            if label == 'boat':  # Assuming 'boat' is the label for boats
-                cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
-                cv2.putText(frame, f'{label} {confidence:.2f}', (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
 
-        out.write(frame)
+            bounding_box_color = CLASS_COLOR_MAP.get(label, (0, 255, 0))
+            cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), bounding_box_color, 2)
+            cv2.putText(frame, f'{label} {confidence:.2f}', (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, bounding_box_color, 2)
+
+        if save_video:
+            out.write(frame)
 
         # Display the frame
         cv2.imshow('Boat Detection', frame)
@@ -87,25 +105,26 @@ def detect_boats_in_video(video_path: str, model_path: str) -> None:
 
     # Release video capture and writer
     cap.release()
-    out.release()
+    if save_video:
+        out.release()
+        print(f"Processed video saved to {output_path}")
     cv2.destroyAllWindows()
-    print(f"Processed video saved to {output_path}")
+    
 
 def main():
     parser = argparse.ArgumentParser(description="Detect boats in images or videos using a trained OD model.")
     parser.add_argument('model', type=str, help="Path to the trained YOLO model (.pt) file")
     parser.add_argument('media', type=str, help="Path to the image or video file")
     parser.add_argument('--type', type=str, choices=['image', 'video'], required=True, help="Type of the media (image or video)")
-    # TODO: Add argument to specify the output directory, whether to save the results to a file or display them
+    parser.add_argument('--save', action='store_true', help="Flag indicating whether to save the results to a file")
+
+    # TODO: Add argument to specify the output directory
     args = parser.parse_args()
 
     if args.type == 'image':
-        detect_boats_in_image(args.media, args.model)
+        detect_boats_in_image(args.media, args.model, args.save)
     elif args.type == 'video':
-        detect_boats_in_video(args.media, args.model)
+        detect_boats_in_video(args.media, args.model, args.save)
 
 if __name__ == "__main__":
     main()
-
-# python detect.py path/to/your/model.pt path/to/your/image.jpg --type image
-# python detect.py path/to/your/model.pt path/to/your/video.mp4 --type video 
